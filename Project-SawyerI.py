bce755577c0075e385d76b19fb78b713b78ddb9b
sawyeria42 <110945041+sawyeria42@users.noreply.github.com>
2022-12-06 12:58:26 -0600

    1

--- Project-SawyerI.py
+++ Project-SawyerI.py
@@ -1,47 +1,130 @@
 # Isaac Sawyer
-# Recipe Calculator and Kitchen Measurement Converter
-# 10/31/2022
+# Recipe Creator/Nutrition Info Application
+# 12/02/2022
 
-# My idea for the final project is a recipe calculator application. This application will prompt the user to enter an ingredient/food and then display
-# information that would commonly be found on a nutrition label (based on 100g of the ingredient and read from file). The user will also be able to enter
-# their own amount of an ingredient and see the nutrition info based on their amount inputted.
-# In addition, the user will be able to add any amount of an ingredient and compile it into a recipe, which will be written to a file. The user will be
-# able to input for saved recipe and have recipe displayed. I would also like to incorporate a measurement converter into this application.
+# This program allows the user to see nutrition info of selected foods/ingredients, and to be able
+# to compile those ingredients together to form a recipe, which is written to a file.
 
-# Declare variables and constants
+# Create arrays
 
+RecipeList = []
+NutritionList = [0.0, 0.0, 0.0, 0.0]
+MasterListForPrompt = ["Chicken Thighs = 1", "Chicken Breast = 2", "Ground Beef = 3", "Chuck Roast = 4"]
+MasterListForFile = ["Chicken Thighs", "Chicken Breast", "Ground Beef", "Chuck Roast"]
 
-# Declare arrays/lists
+# Arrays to hold ingredients of each food family
 
+ChickenList = ["Thighs", "Breast"]
+BeefList = ["Ground Beef, Chuck Roast"]
 
-### Nutrition info based on 100g Module ###
+# Arrays to hold each ingredient's nutrition info
+# Chicken ingredients nutrition
 
+ChickenThighsNutrition = ["Per 100g: Calories: 177, Fat: 8g, Carbs: 0g, Protein: 24g"]
+ChickenThighsParallel = [177.0, 8.0, 0.0, 24.0]
+ChickenBreastNutrition = ["Per 100g: Calories: 165, Fat: 3.6g, Carbs: 0g, Protein: 31g"]
+ChickenBreastParallel = [165.0, 3.6, 0.0, 31.0]
 
-# Prompt user to input an ingredient
+# Beef ingredients nutrition
 
-input = input("Enter an ingredient to see nutrition info: ")
+GroundBeefNutrition = ["Per 100g: Calories: 254, Fat: 20g, Carbs: 0g, Protein: 17g"]
+GroundBeefParallel = [254.0, 20.0, 0.0, 17.0]
+ChuckRoastNutrition = ["Per 100g: Calories: 295, Fat: 19g, Carbs: 0g, Protein: 30g"]
+ChuckRoastParallel = [295.0, 19.0, 0.0, 30.0]
 
-# Open ingredient nutrition data file and display nutrition data
+# Declare variables
+Prompt1 = ""
+Prompt2 = ""
+Prompt3 = ""
 
-with open("Ingredients.txt", "r", encoding="utf-8") as Ingredients:
-        Lines = [line.rstrip("\n") for line in Ingredients]
-if input == "Chicken Thighs":
-    print(Lines[0:4])
-if input == "Ground Beef":
-    print(Lines[5:9])
+# Loop to prompt user for input and hold selection structures for viewing nutrition info
+while Prompt1 != "X" and Prompt1 != "R" and Prompt2 != "X" and Prompt2 != "R":
+    print("Chicken = 1, Beef = 2,")
 
+    Prompt1 = input("Enter number corresponding to ingredient family to see ingredients"
+    " or R to go to recipe creator or X to quit: ")
 
+    # Selection structures for user to choose food family and particular ingredient
+    # Also prints nutrition info for chosen ingredient
 
-### Module to take custom amount of inputted ingredient and calculate/convert from 100g ###
+    if Prompt1 == str(1):
+        print(ChickenList[0] + " = 1, " + ChickenList[1] + " = 2")
+        Prompt2 = input("Enter number corresponding to ingredient to see nutrition info"
+        " or R to go to recipe creator or X to quit: ")
 
+        if Prompt2 == str(1):
+            print(ChickenThighsNutrition)
 
+        if Prompt2 == str(2):
+            print(ChickenBreastNutrition)
 
-### Module for user to add custom amount of an ingredient and write it to a recipe file ###
+    if Prompt1 == str(2):
+        print(BeefList)
+        print("Ground Beef = 1, Chuck Roast = 2")
+        Prompt2 = input("Enter number corresponding to ingredient to see nutrition info"
+        " or R to go to recipe creator or X to quit: ")
 
+        if Prompt2 == str(1):
+            print(GroundBeefNutrition)
 
+        if Prompt2 == str(2):
+            print(ChuckRoastNutrition)
 
-### Module for user to input to have a saved recipe displayed ###
+# Loop to hold selection structures for adding ingredients to recipe
 
+while (Prompt1 == "R" or Prompt2 == "R" or Prompt3 == "R") and Prompt3 != "X":
 
+    print(MasterListForPrompt)
 
-### Module for user to be able to convert between common kitchen measurments ###
+    Prompt3 = input("Enter number corresponding to ingredient to add to recipe or X to quit: ")
+
+# Selection structures for user to select ingredients to add to recipe
+
+    if Prompt3 == str(1):
+
+        RecipeList.append(MasterListForFile[0])
+
+        NutritionList[0] = NutritionList[0] + ChickenThighsParallel[0]
+        NutritionList[1] = NutritionList[1] + ChickenThighsParallel[1]
+        NutritionList[2] = NutritionList[2] + ChickenThighsParallel[2]
+        NutritionList[3] = NutritionList[3] + ChickenThighsParallel[3]
+
+    if Prompt3 == str(2):
+
+        RecipeList.append(MasterListForFile[1])
+
+        NutritionList[0] = NutritionList[0] + ChickenBreastParallel[0]
+        NutritionList[1] = NutritionList[1] + ChickenBreastParallel[1]
+        NutritionList[2] = NutritionList[2] + ChickenBreastParallel[2]
+        NutritionList[3] = NutritionList[3] + ChickenBreastParallel[3]
+
+    if Prompt3 == str(3):
+
+        RecipeList.append(MasterListForFile[2])
+
+        NutritionList[0] = NutritionList[0] + GroundBeefParallel[0]
+        NutritionList[1] = NutritionList[1] + GroundBeefParallel[1]
+        NutritionList[2] = NutritionList[2] + GroundBeefParallel[2]
+        NutritionList[3] = NutritionList[3] + GroundBeefParallel[3]
+
+    if Prompt3 == str(4):
+
+        RecipeList.append(MasterListForFile[3])
+
+        NutritionList[0] = NutritionList[0] + ChuckRoastParallel[0]
+        NutritionList[1] = NutritionList[1] + ChuckRoastParallel[1]
+        NutritionList[2] = NutritionList[2] + ChuckRoastParallel[2]
+        NutritionList[3] = NutritionList[3] + ChuckRoastParallel[3]
+
+# Open and write recipe and nutrition info to file, and close
+
+file = open("Recipe.txt", "a")
+
+for item in RecipeList:
+    file.write(item + ": 100g" + "\n")
+
+file.write("Total Nutrition: " + "\n")
+file.write("Calories: " + str(NutritionList[0]) + " Fat: " + str(NutritionList[1]) +
+               " Carbs: " + str(NutritionList[2]) + " Protein: " + str(NutritionList[3]) + "\n")
+
+file.close()
--- Test.py
+++
@@ -1,5 +0,0 @@
-file = open('Test.txt', 'a')     #open the file named records.txt in write mode
-file.write('hello')                #write the text 'hello' to the file
-file.close()
-
-#Reference: http://easypythondocs.com/fileaccess.html
